dbd88f7de34caa21c983f27ce46bbba7ccd33788
Artem_Mosolov <Artem_Mosolov@epam.com>
2015-08-03 18:06:18 +0300

    removed comment of input point

--- src/main/java/net/habrahabr/Main.java
+++ src/main/java/net/habrahabr/Main.java
@@ -4,18 +4,14 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
-		String name = "*xer/";
-		System.out.println(name.replaceAll("\\*", "Q"));
-		
-		/*try {
+	public static void main(String[] args) {		
+		try {
 			long start = System.currentTimeMillis();
 			new Application().run();
 			long end = System.currentTimeMillis();
 			System.out.println("Time for processing " + (end - start));
 		} catch (IOException e) {
 			e.printStackTrace();
-		}*/
+		}
 	}
 }
